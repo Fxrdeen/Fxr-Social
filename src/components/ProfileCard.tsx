26244a733651ba5ccf22bd28fb3ee20158f773de
Fardeen S <fardeenclan@gmail.com>
2024-07-03 22:53:55 +0530

    added leftsidebar

---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+
+const ProfileCard = () => {
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-6">
+      <div className="h-20 relative">
+        <Image
+          src={"https://picsum.photos/600/600"}
+          alt=""
+          fill
+          className="rounded-md"
+        />
+        <Image
+          src={"https://picsum.photos/400/500"}
+          alt=""
+          width={48}
+          height={48}
+          className="rounded-full w-12 h-12 absolute left-0 right-0 m-auto -bottom-6 ring-1 ring-white z-10"
+        />
+      </div>
+      <div className="h-20 flex flex-col gap-2 items-center">
+        <span className="font-semibold">Fxrdeen Shaikh</span>
+        <div className="flex items-center gap-4">
+          <div className="flex">
+            <Image
+              src={"https://picsum.photos/400/500"}
+              alt=""
+              width={12}
+              height={12}
+              className="rounded-full w-3 h-3 object-cover"
+            />
+            <Image
+              src={"https://picsum.photos/400/500"}
+              alt=""
+              width={12}
+              height={12}
+              className="rounded-full w-3 h-3 object-cover"
+            />
+            <Image
+              src={"https://picsum.photos/400/500"}
+              alt=""
+              width={12}
+              height={12}
+              className="rounded-full w-3 h-3 object-cover"
+            />
+          </div>
+          <span className="text-sm text-gray-500">500 Followers</span>
+        </div>
+        <button className="bg-blue-500 text-white text-xs rounded-md p-2">
+          My Profile
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileCard;
